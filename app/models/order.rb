8cfb88856aa85b9dc6f9650b5d9a82a59a0473cd
Victor Zaffalon <victor.zaffalon@udlab.com.br>
2021-03-10 21:02:41 -0300

    Calculating correctly order total_amount

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,14 +5,14 @@ class Order < ActiveRecord::Base
     accepts_nested_attributes_for :order_items
     accepts_nested_attributes_for :payment
 
-    before_create :set_total_amount
     before_create :create_order_code
+    after_create :set_total_amount
+
+    def create_order_code
+        self.code = SecureRandom.hex(3)
+    end
 
     def set_total_amount
         self.total_amount = self.order_items.sum(:total_amount)
-    end
-
-    def create_order_code
-        self.code = SecureRandom.hex
     end
 end
